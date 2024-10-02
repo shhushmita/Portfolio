920657ec939c5d89255b8800908114d60e0e93b8
Susmita Neupane <shusmitaneupane567@gmail.com>
2024-10-02 21:11:19 +0545

    Update shusmita.js

--- assets/css and js/shusmita.js
+++ assets/css and js/shusmita.js
@@ -11,6 +11,37 @@
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+// Check for saved user preference, if any, on page load
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    body.classList.add(savedTheme);
+}
+
+// Toggle between dark and light themes
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark-theme');
+    } else {
+        localStorage.removeItem('theme');
+    }
+});
+
+// Update icon based on theme
+const icon = toggleButton.querySelector('i');
+function updateIcon() {
+    if (body.classList.contains('dark-theme')) {
+        icon.classList.remove('ti-light-bulb');
+        icon.classList.add('ti-moon');
+    } else {
+        icon.classList.remove('ti-moon');
+        icon.classList.add('ti-light-bulb');
+    }
+}
+updateIcon();
 
 // smooth scroll
 $(document).ready(function(){
